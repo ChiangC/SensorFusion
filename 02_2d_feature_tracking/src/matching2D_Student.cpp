d7fe9809bb73b9f28b61ca4dfedcd9ca181fae17
AI.Tsingh.ChiangC <jiangchuna@126.com>
2020-02-22 15:35:49 +0800

    detKeypointsHarris

--- 02_2d_feature_tracking/src/matching2D_Student.cpp
+++ 02_2d_feature_tracking/src/matching2D_Student.cpp
@@ -105,6 +105,75 @@ void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool b
 // Detect keypoints in image using the traditional Harris  detector
 void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis=false)
 {
+    //Detector parameters
+    int blockSize = 2; //for every pixel, a blockSize x blockSize neigborhood is considered
+    int apertureSize = 3;//aperture parameter for Sobel operator(must be odd)
+    int minResponse = 3; //minimum value for corner in the 8bit scaled response matrix
+    double k = 0.04; //Harris parameter (see equation for details)
+
+    //Detect Harris corners and normalize output
+    cv::Mat dst, dst_norm, dst_norm_scaled;
+    dst = cv::Mat::zeros(img.size(), CV_32FC1);
+    cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
+    cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+
+    if(bVis){
+        //visualize results
+        string windowName = "Harris Corner Detector Response Matrix";
+        cv::nameWindow(windowName, 4);
+        cv::imshow(windowName, dst_norm_scaled);
+        cv::waitKey(0);
+    }
+
+    //Look for prominet corners and instantiate keypoints
+    vector<cv::KeyPoint> keypoints;
+    double maxOverlap = 0.0;//max. permissible overlap between two features in %, used during non-maxima supression 
+    for(size_t j = ; j < dst_norm.rows;j++)
+    {
+        for(size_t i = 0; i< dst_norm.cols; i++)
+        {
+            int response = (int)dst_norm.at<float>(j, i);
+            if(response > minResponse)
+            {
+                //only  store points above a threshold
+                cv::KeyPoint newKeyPoint;
+                newKeyPoint.pt = cv::Point2f(i,j);
+                newKeyPoint.size = 2 * apertureSize;
+                newKeyPoint.response = response;
+
+                //perform non-maximum suppression (NMS) in local neighbourhood around new key point
+                bool bOverlap = false;
+                for(auto it = keypoints.begin(); i != keypoints.end; ++it)
+                {
+                    double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
+                    if(kptOverlap > maxOverlap){
+                        bOverlap = true;
+                        if(newKeyPoint.response > (*it).response)
+                        {                       //if overlap is > AND response is higher for new kpt
+                            *it = newKeyPoint;  //replace old key point with new one
+                            break;//quit loop over keypoints
+                        }
+                    }
+                }
+
+                if(!bOverlap)
+                {                                     //only ad new key point if no overlap has been found in previous NMS
+                    keypoints.push_back(newKeyPoint); //store new keypoint in dynamic list
+                }
+            }
+        }//eof loop over cols
+    }//eof loop over rows
+
+    if(bVis){
+        //visualize keypoints
+        windowName = "Harris Corner Detection Results";
+        cv::namedWindow(windowName, 5);
+        cv::Mat visImage = dst_norm_scaled.clone();
+        cv::drawKeypoints(dst_norm_scaled, keypoints, visImage; cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        cv::imshow(windowName, visImage);
+        cv::waitKey(0);
+    }
 
 }
 
