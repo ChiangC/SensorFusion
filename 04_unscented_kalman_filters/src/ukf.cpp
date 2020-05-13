4440b051eeea206993a12d3dad200178cccc981b
AI.THU.CH.SDC.Quantum <jiangchuna@126.com>
2020-05-13 13:49:16 +0800

    assignment predict update

--- 04_unscented_kalman_filters/src/ukf.cpp
+++ 04_unscented_kalman_filters/src/ukf.cpp
@@ -54,6 +54,8 @@ UKF::UKF() {
    * TODO: Complete the initialization. See ukf.h for other member properties.
    * Hint: one or more values initialized above might be wildly off...
    */
+
+  
 }
 
 UKF::~UKF() {}
